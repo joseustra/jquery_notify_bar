1516350881a6a062d4039068fdc2c58579d4d79e
José Carlos Ustra Júnior <ustrajunior@gmail.com>
2011-07-26 14:01:44 -0300

    refactoring some code

--- lib/jquery_notify_bar/view_helpers.rb
+++ lib/jquery_notify_bar/view_helpers.rb
@@ -6,31 +6,19 @@ module JqueryNotifyBar
       content << "\n#{stylesheet_link_tag('jquery.notifyBar.css')}".html_safe      
     end
 
-    def jquery_notify_bar(opts = {})
+    def jquery_notify_bar(key, msg, opts = {})
       opts.reverse_merge! :flash => :notice, :delay => 2000, :animation_speed => 'normal', :cls => 'notice'
-      html = ''
-      Array.wrap(opts[:flash]).each do |f|
-				if html = flash[f]
-					opts[:cls] ||= f.to_s
-					break
-				end
-			end
-			
-			if html.blank?
-				''
-			else
-        javascript_tag do
-          %($(function () {
-              $.notifyBar({
-                html: '#{html}',
-                delay: #{opts[:delay]},
-                animationSpeed: '#{opts[:animation_speed]}',
-                cls: '#{opts[:cls]}'
-              });  
-            });
-          )
-        end
-      end  
-    end
+      content_tag :div, msg, :class => [key, " message"], :id => "notice_ #{key}"
+      javascript_tag do
+        %($(function () {
+          $.notifyBar({
+            html: '#{msg}',
+            delay: #{opts[:delay]},
+            animationSpeed: '#{opts[:animation_speed]}',
+            cls: '#{opts[:cls]}'
+          });  
+        });)       
+      end
+    end  
   end
 end
